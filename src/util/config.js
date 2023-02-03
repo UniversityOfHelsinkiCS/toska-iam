219904491bc47017c6bb4062ed73cc75bf5dacb9
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-02-03 14:26:17 +0200

    Fix wrong variable name...

--- src/util/config.js
+++ src/util/config.js
@@ -8,7 +8,7 @@ const inStaging = process.env.NODE_ENV === 'staging'
 
 let DB_CONNECTION_STRING = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}?targetServerType=primary`
 
-if (inStaging) DB_CONNECTION_STRING = `${connectionString}&ssl=true`
+if (inStaging) DB_CONNECTION_STRING = `${DB_CONNECTION_STRING}&ssl=true`
 
 module.exports = {
   DB_CONNECTION_STRING,
