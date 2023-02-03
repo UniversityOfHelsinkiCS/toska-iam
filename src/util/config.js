4ca9bb27c4fff245521f3ca986735cb93fe1ae20
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-02-03 14:06:56 +0200

    Use ssl in staging
    Needed for db connection from OpenShift

--- src/util/config.js
+++ src/util/config.js
@@ -20,9 +20,11 @@ const PORT = process.env.PORT
 
 const inProduction = process.env.NODE_ENV === 'production'
 
+const inStaging = process.env.NODE_ENV === 'staging'
+
 let DB_CONNECTION_STRING = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}?targetServerType=primary`
 
-if (process.env.DATABASE_USE_SSL) DB_CONNECTION_STRING = `${connectionString}&ssl=true`
+if (inStaging) DB_CONNECTION_STRING = `${connectionString}&ssl=true`
 
 module.exports = {
   DB_CONNECTION_STRING,
