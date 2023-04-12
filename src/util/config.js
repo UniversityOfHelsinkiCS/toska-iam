ba4fdc4aa665e408f6e702f9a80d17792564a439
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2023-04-12 10:11:52 +0300

    Update db connection method

--- src/util/config.js
+++ src/util/config.js
@@ -4,14 +4,10 @@ const PORT = process.env.PORT
 
 const inProduction = process.env.NODE_ENV === 'production'
 
-const inStaging = process.env.NODE_ENV === 'staging'
-
-let DB_CONNECTION_STRING = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:5432/${process.env.POSTGRES_DATABASE}?targetServerType=primary`
-
-if (inStaging) DB_CONNECTION_STRING = `${DB_CONNECTION_STRING}&ssl=true`
+const DATABASE_URL = process.env.DATABASE_URL
 
 module.exports = {
-  DB_CONNECTION_STRING,
+  DATABASE_URL,
   PORT,
   inProduction,
 }
