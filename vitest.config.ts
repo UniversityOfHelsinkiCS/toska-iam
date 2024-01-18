71461eaeea4ec56edf458d377e1e6563ded364c1
Veikkosuhonen <vesuvesuh@gmail.com>
2024-01-18 23:20:04 +0200

    Configure coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'v8',
+      include: ['src/**/*'],
+      exclude: ['src/db/migrations/**/*'],
+    }
   },
 })
