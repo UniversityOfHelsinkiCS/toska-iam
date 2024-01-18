f74a1f38cb87b6d8525a6700249c36db2f95e094
Veikkosuhonen <vesuvesuh@gmail.com>
2024-01-18 23:53:45 +0200

    Add acual test for kosu

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -24,13 +24,13 @@ test('User with no iam groups gets no access', async () => {
 
 test('User with program manager group gets read access', async () => {
   const res = await api.post('', {
-    userId: 'tkt-jory-member',
-    iamGroups: ['hy-mltdk-tkt-jory'],
+    userId: 'oiktdk-jory-member',
+    iamGroups: ['hy-oiktdk-on-jory'],
   })
 
   expect(res.status).toBe(200)
   expect(await res.json()).toEqual({
-    [mapToDegreeCode('500-K005')]: {
+    [mapToDegreeCode('200-K001')]: {
       read: true,
       write: false,
       admin: false,
@@ -43,13 +43,13 @@ test('User with program manager group gets read access', async () => {
 
 test('User with program manager group and kojo group gets program admin access', async () => {
   const res = await api.post('', {
-    userId: 'tkt-kojo',
-    iamGroups: ['hy-mltdk-tkt-jory', 'hy-mltdk-kandi-kojot'],
+    userId: 'oikis-kojo',
+    iamGroups: ['hy-oiktdk-on-jory', 'hy-oiktdk-kandi-kojot'],
   })
 
   expect(res.status).toBe(200)
   expect(await res.json()).toEqual({
-    [mapToDegreeCode('500-K005')]: {
+    [mapToDegreeCode('200-K001')]: {
       read: true,
       write: true,
       admin: true,
@@ -57,5 +57,91 @@ test('User with program manager group and kojo group gets program admin access',
     specialGroup: {
       jory: true,
     },
+  })
+})
+
+test('Kosu gets READ access to each faculty and programme of location', async () => {
+  const res = await api.post('', {
+    userId: 'kosu',
+    iamGroups: ['hy-ypa-opa-kosu-kruununhaka'],
+  })
+
+  expect(res.status).toBe(200)
+  const access = await res.json()
+
+  // expect(access).toHaveProperty('H10') // TODO: kosu not getting access to faculties
+  // expect(access).toHaveProperty('H20')
+  // expect(access).toHaveProperty('H40')
+  // expect(access).toHaveProperty('H60')
+  // expect(access).toHaveProperty('H70')
+  // expect(access).toHaveProperty('H74')
+
+  // These are the programmes that kruunuhaka kosu should have access to
+  ;[
+    'KH10_001', // teologinen
+    'MH10_001',
+    'T920101',
+
+    'KH20_001', // oikeustieteellinen
+    'MH20_001',
+    'MH20_002',
+    'MH20_003',
+    'T920102',
+
+    'KH40_001', // humanistinen
+    'KH40_002',
+    'KH40_003',
+    'KH40_004',
+    'KH40_005',
+    'KH40_006',
+    'MH40_001',
+    'MH40_002',
+    'MH40_003',
+    'MH40_004',
+    'MH40_005',
+    'MH40_006',
+    'MH40_007',
+    'MH40_008',
+    'MH40_009',
+    'MH40_010',
+    'MH40_011',
+    'MH40_012',
+    'MH40_013',
+    'MH40_014',
+    'MH40_015',
+    'T920103',
+    'T920104',
+    'T920105',
+    'T920111',
+
+    'KH60_001', // kasvatustieteellinen
+    'MH60_001',
+    'MH60_002',
+    'T920109',
+    'T920110',
+
+    'KH70_001', // valtiotieteellinen
+    'KH70_002',
+    'KH70_003',
+    'KH70_004',
+    'MH70_001',
+    'MH70_002',
+    'MH70_003',
+    'MH70_004',
+    'MH70_005',
+    'MH70_006',
+    'MH70_007',
+    'MH70_008',
+    'MH70_009',
+    'MH70_011', // Where is MH70_010? Maybe check.
+    'T920106',
+    'T920107',
+    'T920108',
+
+    'KH74_001', // svenska
+  ].forEach((programme) => {
+    expect(access[programme].read).toBe(true)
+    expect(access[programme].write).toBeFalsy()
+    expect(access[programme].admin).toBeFalsy()
   })
 })
