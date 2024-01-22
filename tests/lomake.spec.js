afb3d3fef4b9ed74cfd1cb0d301b865ec9899754
Veikkosuhonen <vesuvesuh@gmail.com>
2024-01-22 14:48:05 +0200

    Add katselmus related test

---
+++ tests/lomake.spec.js
@@ -0,0 +1,19 @@
+import { describe, expect, it } from "vitest";
+import { api } from "./utils";
+
+describe.concurrent('Lomake', () => {
+  it('Katselmus iam gives the katselmus special group', async () => {
+    const someKatselmusIam = 'grp-katselmus-ltdk'
+
+    const res = await api.post('', {
+      userId: 'katselmus',
+      iamGroups: [someKatselmusIam],
+    })
+  
+    expect(res.status).toBe(200)
+    const json = await res.json()
+    expect(json).toHaveProperty('specialGroup')
+    expect(json.specialGroup).toHaveProperty('katselmusViewer')
+    expect(json.specialGroup.katselmusViewer).toBe(true)
+  })
+})
